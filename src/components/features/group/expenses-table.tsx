1587f978a4ada5be16e9fe1556503a464d6c47b2
Raşid Ağaç <agacrasid@outlook.com>
2025-06-02 17:13:49 +0300

    feat(expenses): BUN-12 - implement swipeable list for expense management
    - Added SwipeableList component to enhance user interaction with expenses.
    - Implemented handleEdit and handleDelete functions for expense items.
    - Refactored ExpensesTable to utilize the new renderItem function for better code organization.

--- src/components/features/group/expenses-table.tsx
+++ src/components/features/group/expenses-table.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { useCallback } from "react";
+
+import { deleteExpense } from "@/actions/expenses/deleteExpense";
+import { SwipeableList } from "@/components/swipeable-list";
 
 type ExpenseWithProfile = {
   id: string;
@@ -17,32 +21,45 @@ interface ExpensesTableProps {
 }
 
 export function ExpensesTable({ data }: ExpensesTableProps) {
-  return (
-    <div>
-      {data.map((expense) => (
-        <div
-          key={expense.id}
-          className="grid grid-cols-[auto_auto_1fr_auto] items-center gap-2 gap-x-4 text-xs"
-        >
-          <div className="w-min text-center">{expense.created_at}</div>
-          <div className="bg-gray-200 p-1">
-            <Image
-              src={expense.image_url || "/window.svg"}
-              width={25}
-              height={25}
-              alt={expense.title}
-            />
-          </div>
-          <div className="flex flex-col">
-            <div className="text-base font-bold">{expense.title}</div>
-            <div className="text-gray-500">{`${expense.full_name} paid ${expense.amount}`}</div>
-          </div>
-          <div className="text-right">
-            <div>{expense.debt.text}</div>
-            <div>{expense.debt.amount}</div>
-          </div>
+  const handleEdit = async (item: ExpenseWithProfile) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("Edit item:", item);
+  };
+
+  const handleDelete = async (id: string) => {
+    await deleteExpense(id);
+  };
+
+  const renderItem = useCallback((expense: ExpenseWithProfile) => {
+    return (
+      <div className="grid grid-cols-[auto_auto_1fr_auto] items-center gap-2 gap-x-2 text-xs">
+        <div className="w-min text-center">{expense.created_at}</div>
+        <div className="bg-gray-200 p-1">
+          <Image
+            src={expense.image_url || "/window.svg"}
+            width={25}
+            height={25}
+            alt={expense.title}
+          />
         </div>
-      ))}
-    </div>
+        <div className="flex flex-col">
+          <div className="text-base font-bold">{expense.title}</div>
+          <div className="text-gray-500">{`${expense.full_name} paid ${expense.amount}`}</div>
+        </div>
+        <div className="text-right">
+          <div>{expense.debt.text}</div>
+          <div>{expense.debt.amount}</div>
+        </div>
+      </div>
+    );
+  }, []);
+
+  return (
+    <SwipeableList
+      data={data}
+      renderItem={renderItem}
+      onEdit={handleEdit}
+      onDelete={handleDelete}
+    />
   );
 }
