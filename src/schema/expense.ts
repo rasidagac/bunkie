4b73e4eacf6a3b14550318b749eeedfd4aa6fbce
Raşid Ağaç <agacrasid@outlook.com>
2025-06-03 02:15:11 +0300

    fix(upload): BUN-15 - update uploadFile function to allow file overwriting
    - Added allowOverwrite option to the uploadFile function for enhanced file management.
    - Updated FileInput component to use File type instead of FileWithPreview for better consistency.
    - Simplified image schema in expense form to directly use File instance without additional properties.

--- src/schema/expense.ts
+++ src/schema/expense.ts
@@ -5,14 +5,5 @@ export const formSchema = z.object({
   amount: z.coerce.number().positive(),
   currency: z.union([z.literal("TRY"), z.literal("USD"), z.literal("EUR")]),
   split_type: z.enum(["equal", "custom", "percentage"]).default("equal"),
-  image: z
-    .array(
-      z.intersection(
-        z.instanceof(File),
-        z.object({
-          id: z.string(),
-        }),
-      ),
-    )
-    .length(1),
+  image: z.array(z.instanceof(File)).length(1),
 });
